01efe71e30aa51fb0ef6822cacc45fb7fc1d3d9b
shymaaismail <shymaa.m.ismail@gmail.com>
2025-05-31 00:58:01 +0300

    Implement PostModal component for adding new posts and update Posts page to handle modal state

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -36,3 +36,16 @@ export interface Company {
     catchPhrase: string;
     bs:          string;
 }
+
+
+export interface PostData {
+    userId: number;
+    id?: number;
+    title: string;
+    body: string;
+}
+
+export interface PostModalProps {
+    onClose: () => void;
+    onSubmit: (post: PostData) => void;
+}
